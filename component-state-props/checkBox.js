d4d4850da321932d83fcf16ceb30f2e6c2be693f
pawelmiklas <pablomiklas@gmail.com>
2019-01-17 23:30:17 +0100

    mini checkout

--- component-state-props/checkBox.js
+++ component-state-props/checkBox.js
@@ -1,16 +1,31 @@
 const Message = (props) => (
     <p>{props.txt}</p>
 )
-const handleCheckCheckbox = (isConfirmed, isFormSubmitted) => {
-    if (isFormSubmitted) {
-        if (isConfirmed) {
-            return <Message txt="Możesz obejrzeć film!" />
-        } else {
-            return <Message txt="Nie masz odpowiedniego wieku!" />
-        }
-    } else { return null }
+// const handleCheckCheckbox = (isConfirmed, isFormSubmitted) => {
+//     if (isFormSubmitted) {
+//         if (isConfirmed) {
+//             return <Message txt="Możesz obejrzeć film!" />
+//         } else {
+//             return <Message txt="Nie masz odpowiedniego wieku!" />
+//         }
+//     } else { return null }
+// }
+const OrderForm = (props) => {
+    const { change, checked, submit } = props;
+    return (
+        <form onSubmit={submit}>
+            <input
+                type="checkbox"
+                id="age"
+                onChange={change}
+                checked={checked}
+            />
+            <label htmlFor="age">Mam więcej niż 16 lat</label>
+            <br />
+            <button type="submit">Kup bilet</button>
+        </form>
+    )
 }
-
 class CheckBox extends React.Component {
     state = {
         isConfirmed: false,
@@ -33,34 +48,28 @@ class CheckBox extends React.Component {
         }
     }
 
-    // handleCheckCheckbox = () => {
-    //     if (this.state.isFormSubmitted) {
-    //         if (this.state.isConfirmed) {
-    //             return <Message txt="Możesz obejrzeć film!" />
-    //         } else {
-    //             return <Message txt="Nie masz odpowiedniego wieku!" />
-    //         }
-    //     } else { return null }
-    // }
+    handleCheckCheckbox = () => {
+        if (this.state.isFormSubmitted) {
+            if (this.state.isConfirmed) {
+                return <Message txt="Możesz obejrzeć film!" />
+            } else {
+                return <Message txt="Nie masz odpowiedniego wieku!" />
+            }
+        } else { return null }
+    }
 
     render() {
         console.log(this.state.isFormSubmitted);
         return (
             <div>
                 <h1>Kup bilet na najlepszy film roku!</h1>
-                <form onSubmit={this.handleCheckSubmit}>
-                    <input
-                        type="checkbox"
-                        id="age"
-                        onChange={this.handleChecbox}
-                        checked={this.isConfirmed}
-                    />
-                    <label htmlFor="age">Mam więcej niż 16 lat</label>
-                    <br />
-                    <button type="submit">Kup bilet</button>
-                </form>
-                {handleCheckCheckbox(this.state.isConfirmed, this.state.isFormSubmitted)}
-                {/* {this.handleCheckCheckbox() */}
+                <OrderForm
+                    change={this.handleChecbox}
+                    submit={this.handleCheckSubmit}
+                    checked={this.state.isConfirmed}
+                />
+                {/* {handleCheckCheckbox(this.state.isConfirmed, this.state.isFormSubmitted)} */}
+                {this.handleCheckCheckbox()}
             </div>
         )
     }
---
+++ component-state-props/miniCheckout.js
@@ -0,0 +1,44 @@
+class MiniCheckout extends React.Component {
+    state = {
+        amount: 0,
+        availableAmout: 12
+    }
+
+    handleChangeAmount = (e) => {
+        if (e === "plus") {
+            this.setState((prevState) => ({
+                amount: prevState.amount + 1
+            }))
+        } else {
+            this.setState((prevState) => ({
+                amount: prevState.amount - 1
+            }))
+        }
+    }
+
+    handleRemoveAvailableAmount = () => {
+        this.setState((prevState) => ({
+            availableAmout: prevState.availableAmout - this.state.amount,
+            amount: prevState.amount = 0
+        }))
+    }
+    render() {
+        const { amount, availableAmout } = this.state;
+        return (
+            <div>
+                <button
+                    onClick={() => this.handleChangeAmount("minus")}
+                    disabled={amount === 0 ? true : false}
+                >-</button>
+                <span style={amount === 0 ? { opacity: 0.3 } : {}}> {amount} </span>
+                <button
+                    onClick={() => this.handleChangeAmount("plus")}
+                    disabled={amount === availableAmout ? true : false}
+                >+</button>
+                {amount > 0 && <button onClick={this.handleRemoveAvailableAmount}>kup!</button>}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<MiniCheckout />, document.getElementById("root"));
